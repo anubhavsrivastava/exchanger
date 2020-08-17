301ddf5f8c6f4c823c0e6b8b01ac3c9838b0ec39
Anubhav Srivastava <anubhavsrivastava@outlook.com>
2020-08-17 23:19:55 +0200

    chore: added overlay shadow

--- src/components/Layout/DefaultLayout/index.tsx
+++ src/components/Layout/DefaultLayout/index.tsx
@@ -8,7 +8,7 @@ type Props = {
 };
 const DefaultLayout: FC<Props> = ({ header, children, footer = "" }) => {
     return (
-        <div>
+        <div className={layoutStyles.default_app_container}>
             <div className={layoutStyles.header}>{header}</div>
             <main className={layoutStyles.default_app_layout}>{children}</main>
             <div className={layoutStyles.footer}>{footer}</div>
